7f7b2535f6c4e4289286e4f6b5427ed47b09aabc
RajatBhat007 <rajat@thegamificationcompany.com>
2023-10-13 13:27:36 +0530

    ui changes

--- src/component/WebViewScreen.jsx
+++ src/component/WebViewScreen.jsx
@@ -1,19 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { BackHandler,Alert} from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Alert, BackHandler, StatusBar, StyleSheet, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import WebView from 'react-native-webview';
+import { WHITE } from './color';
 
-const WebViewScreen = ({ url, userData, onNavigationStateChange }) => {
-    console.log(userData,".......wwww");
-  const [showExitConfirmation, setShowExitConfirmation] = useState(false);
+const WebViewScreen = () => {
 
   useEffect(() => {
     const backAction = () => {
-      if (showExitConfirmation) {
-        return false;
-      } else {
-        setShowExitConfirmation(true);
-        return true;
-      }
+      Alert.alert( 'Exit App',
+      'Are you sure you want to exit?', [
+        {
+          text: 'Cancel',
+          onPress: () => null,
+          style: 'cancel',
+        },
+        {text: 'YES', onPress: () => BackHandler.exitApp()},
+      ]);
+      return true;
     };
 
     const backHandler = BackHandler.addEventListener(
@@ -21,37 +25,42 @@ const WebViewScreen = ({ url, userData, onNavigationStateChange }) => {
       backAction,
     );
 
-    return () => backHandler.remove(); // Cleanup event listener on unmount
-  }, [showExitConfirmation]);
+    return () => backHandler.remove();
+  }, []);
+  const webViewRef = useRef(null);
 
-  const handleExitConfirmation = () => {
-    // Handle exit confirmation actions if needed
-    setShowExitConfirmation(false);
+  // Create a function to handle Android hardware back button behavior within WebView
+  const handleAndroidBackButton = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true; // WebView handled the back press
+    }
+    return false; // WebView can't go back, handle back press for your app navigation
   };
 
   return (
-    <>
-      <WebView
-        source={{ uri: url }}
-        onNavigationStateChange={onNavigationStateChange}
-      />
-
-      {showExitConfirmation && (
-        Alert.alert(
-            "Hold on",
-            "Please use the app back button or home button to navigate",
-            [
-              {
-                text: "OK",
-                onPress: handleExitConfirmation,
-                style: "cancel"
-              },
-  
-            ]
-          )
-      )}
-    </>
+    <SafeAreaProvider>
+      <StatusBar backgroundColor={WHITE} barStyle="dark-content" />
+      <View style={styles.container}>
+        <WebView
+          ref={webViewRef}
+          source={{ uri: 'https://www.m2ost.in/m2ostconsole/' }}
+          onNavigationStateChange={(navState) => {
+            if (navState.canGoBack) {
+              // WebView can go back, handle the back navigation
+              webViewRef.current.goBack();
+            }
+          }}
+        />
+      </View>
+    </SafeAreaProvider>
   );
 };
 
 export default WebViewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
