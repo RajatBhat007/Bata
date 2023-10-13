83569d63f80bc33d390f08f132334f229ee13a61
RajatBhat007 <rajat@thegamificationcompany.com>
2023-10-13 12:43:02 +0530

    ui changes

--- src/component/color.js
+++ src/component/color.js
@@ -3,3 +3,6 @@ export const BGRED='#E60000';
 export const GRAY_BLACK = "#474444";
 export const BLACK = '#000000';
 export const DARK_BLACK="#07252D"
+export const BLUE = '#1D2149';
+export const MDGREEN = "#99C939";
+export const DGREEN = "#046465"
--- src/component/environment.js
+++ src/component/environment.js
@@ -1,3 +1,3 @@
-// export const URL = "https://www.m2ost.in/m2ostconsole/"
+export const URL = "https://www.m2ost.in/m2ostconsole/"
 export const bataUrllogin="https://www.m2ost.in/m2ostconsole/Account/LoginAPICheck"
 export const Url="https://www.m2ost.in/m2ostconsole/Account/LoginAPICheck_Response"
